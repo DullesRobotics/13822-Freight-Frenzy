267f6be5881c689ea74e4747b6ff6da9a1edb6cb
Xcallibur232 <elliotmichlin@gmail.com>
2021-10-07 14:18:34 -0500

    Revert "Removed TeamCode"
    This reverts commit c25cf669209c13ad757e012ce09ba1259f99065b.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tolerance/OpModes/TeleOpModes/MecDriveTest.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.Tolerance.OpModes.TeleOpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.RobotManager.MecanumDriveTrain;
+import org.firstinspires.ftc.teamcode.Tolerance.Configurator;
+
+@TeleOp
+public class MecDriveTest extends LinearOpMode {
+
+    private MecanumDriveTrain robot;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot = new MecanumDriveTrain(this);
+        robot.addHardware(Configurator.getDriveTrainMotors(robot));
+
+        waitForStart();
+
+        robot.driveWithController(robot.ctrl1());
+
+        while (opModeIsActive())
+            robot.getLogger().updateLog();
+
+        robot.stopAllThreads();
+    }
+}
